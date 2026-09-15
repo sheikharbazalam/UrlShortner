73e3e2e68fbe981a13eda09a773b7ea5372ac775
Sheikh Arbaz Alam <sheikharbazalam@Mac.mynet>
2026-09-15 23:12:49 +0100

    feat: initial URL shortener project - Spring Boot + Redis + AWS

---
+++ src/main/java/com/learner/urlshortener/model/Url.java
@@ -0,0 +1,32 @@
+package com.learner.urlshortener.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "urls")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Url {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String originalUrl;
+
+    @Column(nullable = false, unique = true)
+    private String shortCode;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    private Long clickCount = 0L;
+}
